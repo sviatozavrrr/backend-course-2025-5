766ad49e4de05e9f951818d08a30d03a27993749
Святозар bc2024-5 <sviatozar007@gmail.com>
2025-11-13 12:51:28 +0200

    Встановлено commander.js, superagent, nodemon. додано .gitignore, реалізовано читання аргументів

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Примітивний кешуючий проксі-сервер запускається...");
+const { Command } = require('commander');
+const program = new Command();
+
+// Описуємо аргументи командного рядка
+program
+  .requiredOption('-h, --host <host>', 'адреса сервера')
+  .requiredOption('-p, --port <port>', 'порт сервера')
+  .requiredOption('-c, --cache <path>', 'шлях до директорії кешу')
+  .parse(process.argv);
+
+// Отримаємо параметри
+const options = program.opts();
+console.log('Параметри командного рядка:', options);
